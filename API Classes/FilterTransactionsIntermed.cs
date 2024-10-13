7471061c959685065fd695c903aca262c8a65c28
Jin Lee <21479694@student.curtin.edu.au>
2024-10-13 21:56:41 +0800

    Modified FilterTransactions to accept an admin username

--- API Classes/FilterTransactionsIntermed.cs
+++ API Classes/FilterTransactionsIntermed.cs
@@ -8,19 +8,22 @@
             this.maxAmount = 0;
             this.orderType = "";
             this.username = "";
+            this.adminUsername = "";
         }
 
-        public FilterTransactionsIntermed(int minAmount, int maxAmount, string transactionType, string username)
+        public FilterTransactionsIntermed(int minAmount, int maxAmount, string transactionType, string username, string adminUsername)
         {
             this.minAmount = minAmount;
             this.maxAmount = maxAmount;
             this.orderType = transactionType;
             this.username = username;
+            this.adminUsername = adminUsername;
         }
 
         public int? minAmount { get; set; }
         public int? maxAmount { get; set; }
         public string orderType { get; set; }
         public string username { get; set; }
+        public string adminUsername { get; set; }
     }
 }
